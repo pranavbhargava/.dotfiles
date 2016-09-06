d4b0eb7d4d7a9b452834d9f6eebc96638140e069
Zach Holman <zach@zachholman.com>
2016-09-06 14:37:45 -0700

    Don't print +/- signs in diff output
    I copy and paste so often from the output of `git diff` that it became
    really tedious to manually remove the diff output from the start of a
    line:
    
        - runBuggyCode()
        + runVerySlightlyLessBuggyCode()
    
    This is able to be done and keep my sanity because I have `diff=auto`
    set in my `[color]` section of my gitconfig, so I can just rely on
    green/red output instead of the added +/- markers.
    
    If I do want the +/- for things like gists or passing around diffs, I
    can just run the normal `git diff`.
    
    A poor (wo)man's way of doing this is to use `git diff --color-words`,
    but I opted not to do that because it does slightly change the format
    (changes are inline instead of per-line).

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,8 +10,11 @@ fi
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
-alias gd='git diff'
 alias gundo='git reset --hard'
+
+# Remove `+` and `-` from start of diff lines; just rely upon color.
+alias gd='git diff --color | sed -E "s/^([^-+ ]*)[-+ ]/\\1/" | less -r'
+
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
