8cae3980af34727f5e8d0b7361f572cd75b0639b
Pranav Bhargava <pranav@jobvite-inc.com>
2017-01-22 00:53:32 -0800

    Updated config to point to the right auto suggestions.

--- antigen/config.zsh
+++ antigen/config.zsh
@@ -2,5 +2,5 @@ source $ZSH/antigen/antigen.zsh
 
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-history-substring-search ./zsh-history-substring-search.zsh
+antigen bundle zsh-users/zsh-autosuggestions
 
-antigen bundle tarruda/zsh-autosuggestions
